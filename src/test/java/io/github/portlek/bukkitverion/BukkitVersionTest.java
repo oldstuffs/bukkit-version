8c4960290c1b03bb4c6794d46bc1f12c81a528ea
Hasan Demirtaş <utsukushihito@outlook.com>
2020-10-22 14:56:25 +0300

    Update BukkitVersionTest.java

--- src/test/java/io/github/portlek/bukkitverion/BukkitVersionTest.java
+++ src/test/java/io/github/portlek/bukkitverion/BukkitVersionTest.java
@@ -1,9 +1,9 @@
 package io.github.portlek.bukkitverion;
 
 import io.github.portlek.bukkitversion.BukkitVersion;
-import org.hamcrest.MatcherAssert;
 import org.hamcrest.core.IsEqual;
 import org.junit.jupiter.api.Test;
+import org.llorllale.cactoos.matchers.Assertion;
 
 final class BukkitVersionTest {
 
@@ -13,37 +13,37 @@ final class BukkitVersionTest {
 
   @Test
   void getVersion() {
-    MatcherAssert.assertThat(
+    new Assertion<>(
       "Cannot get the raw version",
       BukkitVersionTest.VERSION.getVersion(),
       new IsEqual<>(BukkitVersionTest.MC_VERSION)
-    );
+    ).affirm();
   }
 
   @Test
   void major() {
-    MatcherAssert.assertThat(
+    new Assertion<>(
       "Cannot get the major  version",
       BukkitVersionTest.VERSION.major(),
       new IsEqual<>(1)
-    );
+    ).affirm();
   }
 
   @Test
   void minor() {
-    MatcherAssert.assertThat(
+    new Assertion<>(
       "Cannot get the minor version",
       BukkitVersionTest.VERSION.minor(),
       new IsEqual<>(8)
-    );
+    ).affirm();
   }
 
   @Test
   void micro() {
-    MatcherAssert.assertThat(
+    new Assertion<>(
       "Cannot get the micro version",
       BukkitVersionTest.VERSION.micro(),
       new IsEqual<>(3)
-    );
+    ).affirm();
   }
 }
